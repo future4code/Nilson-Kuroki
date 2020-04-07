d290702a315624fef03d349caa25a5026775f18f
NilsonKuroki <nilsonkurokii@gmail.com>
2020-04-07 17:40:00 -0300

    atualizando

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,5 +1,5 @@
-let informacao = document.getElementById("informacao").value
-let diaDaSemana = document.getElementById("diaDaSemana").value
+let informacao = document.getElementById("informacao")
+let diaDaSemana = document.getElementById("diaDaSemana")
 let segundaFeira = document.getElementById("segunda-feira")
 let tercaFeira = document.getElementById("terca-feira")
 let quartaFeira = document.getElementById("quarta-feira")
@@ -7,29 +7,37 @@ let quintaFeira = document.getElementById("quinta-feira")
 let sextaFeira = document.getElementById("sexta-feira")
 let sabado = document.getElementById("sabado")
 let domingo = document.getElementById("domingo")
+const buttonClear = document.getElementById("button-clear")
 
 function inserirCronograma(){
-    if(informacao === ""){
+    console.log(informacao)
+    if(informacao.value === ""){
         alert("Digite algum valor!")
     }else{
-        if(diaDaSemana === "segunda-feira"){
-            segundaFeira.innerHTML += "<li onclick='riscar(this)'>"+informacao+"</li>"
-        }else if(diaDaSemana === "terca-feira"){
-            tercaFeira.innerHTML += "<li onclick='riscar(this)'>"+informacao+"</li>"
-        }else if(diaDaSemana === "quarta-feira"){
-            quartaFeira.innerHTML += "<li onclick='riscar(this)'>"+informacao+"</li>"
-        }else if(diaDaSemana === "quinta-feira"){
-            quintaFeira.innerHTML += "<li onclick='riscar(this)'>"+informacao+"</li>"
-        }else if(diaDaSemana === "sexta-feira"){
-            sextaFeira.innerHTML += "<li onclick='riscar(this)'>"+informacao+"</li>"
-        }else if(diaDaSemana === "sabado"){
-            sabado.innerHTML += "<li onclick='riscar(this)'>"+informacao+"</li>"
-        }else if(diaDaSemana === "domingo"){
-            domingo.innerHTML += "<li onclick='riscar(this)'>"+informacao+"</li>"
+        if(diaDaSemana.value === "segunda-feira"){
+            segundaFeira.innerHTML += "<li onclick='riscar(this)'>"+informacao.value +"</li>"
+        }else if(diaDaSemana.value  === "terca-feira"){
+            tercaFeira.innerHTML += "<li onclick='riscar(this)'>"+informacao.value +"</li>"
+        }else if(diaDaSemana.value  === "quarta-feira"){
+            quartaFeira.innerHTML += "<li onclick='riscar(this)'>"+informacao.value +"</li>"
+        }else if(diaDaSemana.value  === "quinta-feira"){
+            quintaFeira.innerHTML += "<li onclick='riscar(this)'>"+informacao.value +"</li>"
+        }else if(diaDaSemana.value  === "sexta-feira"){
+            sextaFeira.innerHTML += "<li onclick='riscar(this)'>"+informacao.value +"</li>"
+        }else if(diaDaSemana.value  === "sabado"){
+            sabado.innerHTML += "<li onclick='riscar(this)'>"+informacao.value +"</li>"
+        }else if(diaDaSemana.value  === "domingo"){
+            domingo.innerHTML += "<li onclick='riscar(this)'>"+informacao.value +"</li>"
         }
     }
+    informacao.value = ""
 }
 
 function riscar(tudo){
     tudo.style.textDecoration = "line-through";
+}
+
+function limpar(){
+    segundaFeira.value = ""
+    tercaFeira.value = ""
 }

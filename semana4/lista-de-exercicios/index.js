d3025ed656c033d069eb55da20a48fb87fd770b6
NilsonKuroki <nilsonkurokii@gmail.com>
2020-04-08 20:18:06 -0300

    exercicios de leitura exercicio 3 feito

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -4,5 +4,15 @@ mostra um codigo de conversão de dinheiro de dolar para real, uma function
 para receber o valorem dolar e devolver com o preço convertido em real,
 aplicando a conversão
 
+exercicio 2
+um function que recebe dois paramentros que entra em um switch para escolher o tipo de investimento
+e quanto quer aplicar, apos ser identificado pelo case, é feito aplicação do novo montante e o 
+segundo montante sendo mostrado no console.log
+
+exercicio 3
+são declarados 3 arrays um chamado numeros que possue valores dentro e os outros dois vazios, if os
+valores encontrados no vetor numero for par, essse valor será inteirado no array1, caso seja impar
+será parte do array2
+
 
 */

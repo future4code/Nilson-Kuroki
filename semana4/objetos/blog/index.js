4d278beeada7f0fa156a80fdbcb654f4bc907bc1
NilsonKuroki <nilsonkurokii@gmail.com>
2020-04-08 19:08:40 -0300

    exercicio feito, menos o desafio

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,17 +1,7 @@
 let blocosDePosts = {}
 const arrayPost = []
 
-const posts = document.getElementById("posts")
-let variavelQualquer = ''
-
-function postar(blocosDePosts){
-    posts.innerHTML += 
-        "<p>Autor: "+blocosDePosts.autor+"</p>"+
-        "<h4>"+blocosDePosts.titulo+"</h4>"+
-        "<p>"+blocosDePosts.texto+"</p>"+variavelQualquer
-}
-
-function novaFuncao(){
+function armazenar(){
     const autor = document.getElementById("autor")
     const titulo = document.getElementById("titulo")
     const texto = document.getElementById("texto")
@@ -25,5 +15,15 @@ function novaFuncao(){
     titulo.value = ""
     texto.value = ""
     console.log(arrayPost)
-    
+    postar(blocosDePosts)
+}
+
+function postar(blocosDePosts){
+    let posts = document.getElementById("posts")
+    let variavelQualquer = posts.innerHTML
+    posts.innerHTML = 
+        "<p>Autor: "+blocosDePosts.autor+"</p>"+
+        "<h4>"+blocosDePosts.titulo+"</h4>"+
+        "<p class='comentario'>"+blocosDePosts.texto+"</p>"+
+        variavelQualquer
 }

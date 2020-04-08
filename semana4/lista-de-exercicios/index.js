1faa12a4ae2353b5d1b07d5c6426ddd71456166d
NilsonKuroki <nilsonkurokii@gmail.com>
2020-04-08 20:16:50 -0300

    exercicio 1 feito

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,8 @@
+/*leitura de codigo
+exercicio 1
+mostra um codigo de conversão de dinheiro de dolar para real, uma function
+para receber o valorem dolar e devolver com o preço convertido em real,
+aplicando a conversão
+
+
+*/

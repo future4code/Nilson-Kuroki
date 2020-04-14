d9d7f0cd66a34d36d5eb2e21b1de34482cf8ffcf
NilsonKuroki <nilsonkurokii@gmail.com>
2020-04-14 16:33:56 -0300

    atividade realizada

---
+++ semana5/lab-tube/build/precache-manifest.e9b1f51d4b2700e4d517781ef76d6908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66755814f085a8126d8326df69001c6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcde57737e6824e50ac9",
+    "url": "/static/css/main.7073c9df.chunk.css"
+  },
+  {
+    "revision": "9257fcc5d500e48c60e4",
+    "url": "/static/js/2.f5dffba1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f5dffba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcde57737e6824e50ac9",
+    "url": "/static/js/main.5671fdfe.chunk.js"
+  },
+  {
+    "revision": "86eeef2b77b4a541b6ec",
+    "url": "/static/js/runtime-main.be69ecd0.js"
+  }
+]);

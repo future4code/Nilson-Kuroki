797925e922a17838d09a2ad4dd9e846b05aa1ea8
NilsonKuroki <nilsonkurokii@gmail.com>
2020-04-16 17:01:18 -0300

    aula 20

--- semana5/insta4 2/src/App.js
+++ semana5/insta4 2/src/App.js
@@ -3,24 +3,40 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+  state = {
+    pubs: [
+      {
+      nomeUsuario:'paulinha',
+      fotoUsuario:'https://picsum.photos/50/50?a=1',
+      fotoPost:'https://picsum.photos/200/150?a=2'
+      },
+      {
+      nomeUsuario:'Nilson',
+      fotoUsuario:'https://picsum.photos/50/50?a=3',
+      fotoPost:'https://picsum.photos/200/150?a=4'
+      },
+      {
+      nomeUsuario:'Nana',
+      fotoUsuario:'https://picsum.photos/50/50?a=5',
+      fotoPost:'https://picsum.photos/200/150?a=6'
+      }
+    ]
+  }
+
   render() {
+    const listaDePublicacao = this.state.pubs.map(pub =>{
+      return(
+        <Post
+          nomeUsuario = {pub.nomeUsuario}
+          fotosUsuario = {pub.fotosUsuario}
+          fotosPost = {pub.fotoPost}
+        />
+      )
+    })
+
     return (
       <div className={'app-container'}>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50?a=1'}
-          fotoPost={'https://picsum.photos/200/150?a=2'}
-        />
-        <Post
-          nomeUsuario={'Nilson'}
-          fotoUsuario={'https://picsum.photos/50/50?a=3'}
-          fotoPost={'https://picsum.photos/200/150?a=4'}
-        />
-        <Post
-          nomeUsuario={'Gabe'}
-          fotoUsuario={'https://picsum.photos/50/50?a=5'}
-          fotoPost={'https://picsum.photos/200/150?a=6'}
-        />
+        <div>{listaDePublicacao}</div>
       </div>
     );
   }

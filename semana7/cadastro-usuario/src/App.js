d59b7f091eb10c73bbd03912302630bcc1e2a4db
NilsonKuroki <nilsonkurokii@gmail.com>
2020-04-29 16:10:39 -0300

    consegui fazer a atividade e o primeiro item do desafio, mantive tudo no mesmo component para entender melhor as coisas, vou tentar separar em outros components

--- semana7/cadastro-usuario/src/App.js
+++ semana7/cadastro-usuario/src/App.js
@@ -3,6 +3,12 @@ import styled from 'styled-components'
 import axios from 'axios'
 import './App.css';
 
+const ContainerPages = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
 const ContainerCadastro = styled.div`
   display: flex;
   align-items: center;
@@ -13,11 +19,21 @@ const ContainerCadastro = styled.div`
   padding: 10px;
   border: 1px solid black;
 `
+const ContainerNames = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const ContainerListNames = styled.div`
+
+`
+
 class App extends React.Component {
   state= {
     usuarios:[],
     nameValue: "",
-    emailValue: ""
+    emailValue: "",
+    pageAtual: "CadasterUsers"
   }
 
   componentDidMount(){
@@ -42,9 +58,11 @@ class App extends React.Component {
       }
     }).then((resposta)=> {
       console.log("uhu deu certo!", resposta.data)
+      this.pegaUsuario()
     }).catch((resposta)=>{
       console.log("Algo deu errado", resposta)
     })
+    this.setState({nameValue: '', emailValue: ''})
   }
 
   pegaUsuario = () =>{
@@ -58,23 +76,58 @@ class App extends React.Component {
       console.log("deu erro")
     })
   }
+
+  apagarUser = (userId)=>{
+
+    if(window.confirm("Tem certeza que deseja apagar?")){
+      axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${userId}`,{
+      headers:{
+        authorization: 'Nilson-Kuroki-Julian'
+      }
+    }).then((resposta)=> {
+      console.log(resposta)
+      this.pegaUsuario()
+    }).catch((error)=> {
+      console.log(error)
+    })
+    }else{
+      window.alert("Voce nao excluiu!")
+    }
+  }
+  onClickPage = () => {
+    const nextPage = this.state.pageAtual === "CadasterUsers"? "ListUsers": "CadasterUsers"
+    this.setState({pageAtual: nextPage})
+  }
   render(){
     return (
-      <div>
-        <ContainerCadastro>
+      <ContainerPages>
+        <button onClick={this.onClickPage}>
+          {this.state.pageAtual === "CadasterUsers"? "Ir para Lista de usuarios": "Cadastrar usuario"}
+        </button>
+        {this.state.pageAtual === "CadasterUsers"? (
+          <ContainerCadastro>
           <label>Nome:</label>
           <input value = {this.state.nameValue} onChange={this.onChangeNome}/>
           <label>E-mail:</label>
           <input value = {this.state.emailValue} onChange = {this.onChangeEmail}/>
           <button onClick={this.criarUsuario}>Enviar</button>
-
-          <div>
-            {this.state.usuarios.map((usuario)=>{
-              return <p>{usuario.name}</p>
-            })}
-          </div>
         </ContainerCadastro>
-    </div>
+        ):(
+        <ContainerListNames>
+          {this.state.usuarios.map((user)=>{
+              return(
+                <ContainerNames>
+                  <p>{user.name}</p>
+                  <button onClick={()=>{this.apagarUser(user.id)}}>apagar</button>
+                </ContainerNames>
+                
+              ) 
+            })
+          }
+        </ContainerListNames>)}
+        
+        
+    </ContainerPages>
       
     );
   }

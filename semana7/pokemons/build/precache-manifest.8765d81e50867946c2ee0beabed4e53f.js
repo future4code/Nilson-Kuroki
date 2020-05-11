2e6d8a402659d329ac8815127d41911abfddca5c
NilsonKuroki <nilsonkurokii@gmail.com>
2020-05-11 16:44:08 -0300

    Adicionei o novo arquivo aqui e acabei adicionando tambem alterações da semana passada

---
+++ semana7/pokemons/build/precache-manifest.8765d81e50867946c2ee0beabed4e53f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad675ae4ec932c642fbe5f8e91e7ee1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d89f9e732e8f5849382",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fcab99699f8a3fc1a0a5",
+    "url": "/static/js/2.6e5c8599.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6e5c8599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d89f9e732e8f5849382",
+    "url": "/static/js/main.eae53f1b.chunk.js"
+  },
+  {
+    "revision": "3c972f8c891d56148d37",
+    "url": "/static/js/runtime-main.934f165f.js"
+  }
+]);

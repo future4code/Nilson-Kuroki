4f667dfeb2efd31f6fe3e3bba2711ef7286b194c
NilsonKuroki <nilsonkurokii@gmail.com>
2020-05-12 16:43:24 -0300

    exercicio feito!

---
+++ semana8/pokeApi-hooks/build/precache-manifest.2f58385d52ad05700b2aee085457e05f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662306461f591530759ce3e65f22b188",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d64dcefdd1a67e103ee0",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "45cede589c7932f72846",
+    "url": "/static/js/2.21c56684.chunk.js"
+  },
+  {
+    "revision": "d64dcefdd1a67e103ee0",
+    "url": "/static/js/main.d912db0f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

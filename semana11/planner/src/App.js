a30a36b62a14ff5ef34f2bb1528ed8ebfe66cf6e
NilsonKuroki <nilsonkurokii@gmail.com>
2020-06-05 18:57:11 -0300

    gerei alguns teste mas não terminei todos...

--- semana11/planner/src/App.js
+++ semana11/planner/src/App.js
@@ -28,18 +28,41 @@ const Week = styled.div`
 function App() {
   const [input, setInput] = useState("")
   const [select, setSelect] = useState("Segunda-feira")
-  const [segunda, setSegunda] = useState([])
-  const [terca, setTerca] = useState([])
-  const [quarta, setQuarta] = useState([])
-  const [quinta, setQuinta] = useState([])
-  const [sexta, setSexta] = useState([])
-  const [sabado, setSabado] = useState([])
-  const [domingo, setDomingo] = useState([])
-
+  const [tarefas, setTarefas] = useState([])
+  
+  const listaTarefas = ()=>{
+    axios.get('https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-julian-nilson')
+    .then(response=>{
+      console.log(response.data)
+      setTarefas(response.data)
+    })
+  }
 
   useEffect(()=>{
-    axios.get('https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-julian-nilson')
+    listaTarefas()
   }, [])
+
+  const segunda = tarefas.filter((tarefa)=>{
+    return tarefa.day === "Segunda-feira"
+  })
+  const terca = tarefas.filter((tarefa)=>{
+    return tarefa.day === "Terca-feira"
+  })
+  const quarta = tarefas.filter((tarefa)=>{
+    return tarefa.day === "Quarta-feira"
+  })
+  const quinta = tarefas.filter((tarefa)=>{
+    return tarefa.day === "Quinta-feira"
+  })
+  const sexta = tarefas.filter((tarefa)=>{
+    return tarefa.day === "Sexta-feira"
+  })
+  const sabado = tarefas.filter((tarefa)=>{
+    return tarefa.day === "Sabado"
+  })
+  const domingo = tarefas.filter((tarefa)=>{
+    return tarefa.day === "Domingo"
+  })
 
   const onChangeInput = (event)=>{
     setInput(event.target.value)
@@ -50,35 +73,21 @@ function App() {
   }
 
   const salvar = ()=>{
-    switch(select){
-      case "Segunda-feira":
-      setSegunda([...segunda, {id: Date.now(), afazer: input, dia: select}])
-      break;
-      case "Terca-feira":
-      setTerca([...terca, {id: Date.now(), afazer: input, dia: select}])
-      break;
-      case "Quarta-feira":
-      setQuarta([...quarta, {id: Date.now(), afazer: input, dia: select}])
-      break;
-      case "Quinta-feira":
-      setQuinta([...quinta, {id: Date.now(), afazer: input, dia: select}])
-      break;
-      case "Sexta-feira":
-      setSexta([...sexta, {id: Date.now(), afazer: input, dia: select}])
-      break;
-      case "Sabado":
-      setSabado([...sabado, {id: Date.now(), afazer: input, dia: select}])
-      break;
-      case "Domingo":
-      setDomingo([...domingo, {id: Date.now(), afazer: input, dia: select}])
-      break;
+    const body = {
+      text: input,
+      day: select
     }
+
+    axios.post('https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-julian-nilson', body, {
+      headers: {'content-type': 'application/json'}
+    })
+    listaTarefas()
   }
 
   return (
     <div>
       <ContainerForm>
-        <TextField label="comentário" value={input} onChange={onChangeInput} required/>
+        <TextField label="comentario" value={input} onChange={onChangeInput} required/>
         <Select  value={select} onChange={onChangeSelect}>
           <MenuItem value="Segunda-feira">Segunda-feira</MenuItem>
           <MenuItem value="Terca-feira">Terça-feira</MenuItem>
@@ -92,45 +101,45 @@ function App() {
       </ContainerForm>
       <ContainerWeek>
         <Week>
-          <h3>Segunda-feira</h3>
-          {segunda.map(tarefaSegunda=>{
-            return <p>{tarefaSegunda.afazer}</p>
+          <h3>segunda-feira</h3>
+          {segunda.map(tarefa=>{
+            return <p>{tarefa.text}</p>
           })}
         </Week>
         <Week>
-          <h3>Terça-feira</h3>
-          {terca.map(tarefaTerca=>{
-            return <p>{tarefaTerca.afazer}</p>
+          <h3>terca-feira</h3>
+          {terca.map(tarefa=>{
+            return <p>{tarefa.text}</p>
           })}
         </Week>
         <Week>
-          <h3>Quarta-feira</h3>
-          {quarta.map(tarefaQuarta=>{
-            return <p>{tarefaQuarta.afazer}</p>
+          <h3>quarta-feira</h3>
+          {quarta.map(tarefa=>{
+            return <p>{tarefa.text}</p>
           })}
         </Week>
         <Week>
-          <h3>Quinta-feira</h3>
-          {quinta.map(tarefaQuinta=>{
-            return <p>{tarefaQuinta.afazer}</p>
+          <h3>quinta-feira</h3>
+          {quinta.map(tarefa=>{
+            return <p>{tarefa.text}</p>
           })}
         </Week>
         <Week>
-          <h3>Sexta-feira</h3>
-          {sexta.map(tarefaSexta=>{
-            return <p>{tarefaSexta.afazer}</p>
+          <h3>sexta-feira</h3>
+          {sexta.map(tarefa=>{
+            return <p>{tarefa.text}</p>
           })}
         </Week>
         <Week>
-          <h3>Sabado</h3>
-          {sabado.map(tarefaSabado=>{
-            return <p>{tarefaSabado.afazer}</p>
+          <h3>sabado</h3>
+          {sabado.map(tarefa=>{
+            return <p>{tarefa.text}</p>
           })}
         </Week>
         <Week>
-          <h3>Domingo</h3>
-          {domingo.map(tarefaDomingo=>{
-            return <p>{tarefaDomingo.afazer}</p>
+          <h3>domingo</h3>
+          {domingo.map(tarefa=>{
+            return <p>{tarefa.text}</p>
           })}
         </Week>
       </ContainerWeek>
--- semana11/planner/src/App.test.js
+++ semana11/planner/src/App.test.js
@@ -1,9 +1,57 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, wait, fireEvent, getByTestId } from '@testing-library/react';
 import App from './App';
+import axios from 'axios'
+import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+axios.get = jest.fn().mockResolvedValue({data: []})
+axios.post = jest.fn().mockResolvedValue()
+
+test('checa se o botao existe na tela', ()=>{
+  const {getByText} = render(<App/>)
+  expect(getByText(/Salvar/i)).toBeInTheDocument()
+
+})
+
+test('checa input', )
+
+test('requisição pelas tarefas feita', ()=>{
+  render(<App/>)
+
+  expect(axios.get).toHaveBeenCalled()
+
+})
+
+test('requisição pelas tarefas é executada e mostrada', async()=>{
+  axios.get = jest.fn().mockResolvedValue({data: [
+    {
+      text: "test",
+      day: "Segunda-feira"
+    }
+  ]})
+
+  const {findByText} = render(<App/>)
+
+  const tarefa = await findByText('test')
+  expect(tarefa).toBeInTheDocument()
+
+})
+
+test('fazer uma requisição post para a api guardando o dado "tarefa teste"', async ()=>{
+  const {getByLabelText, getByText} = render(<App/>)
+
+  const tarefa = getByLabelText("comentario")
+
+  await userEvent.type(tarefa, "tarefa teste")
+
+  const button = getByText(/Salvar/)
+
+  userEvent.click(button)
+
+  expect(axios.post).toHaveBeenCalledWith('https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-julian-nilson',{
+    text: 'tarefa teste',
+    day: 'Segunda-feira'
+  })
+}
+)
